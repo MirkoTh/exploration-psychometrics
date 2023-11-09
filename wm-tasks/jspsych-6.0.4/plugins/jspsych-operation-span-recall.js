99356335f1058cf9f697b17f851eb247d173c873
Mirko Thalmann <mirkothalmann@hotmail.com>
2023-11-09 19:45:43 +0100

    implement feedback from lab pilot:

--- wm-tasks/jspsych-6.0.4/plugins/jspsych-operation-span-recall.js
+++ wm-tasks/jspsych-6.0.4/plugins/jspsych-operation-span-recall.js
@@ -247,6 +247,7 @@ jsPsych.plugins["operation-span-recall"] = (function () {
         saveData(JSON.stringify(data_recall_clean), file_name, "OS");
         var file_name_cum = "OS_recall_allinone_" + trial.participant_id + ".json";
         saveSeveralDataOverwrite(data_cumulative, file_name_cum, "OS");
+        setTimeout(undefined, 1000);
       }
     }
   };
