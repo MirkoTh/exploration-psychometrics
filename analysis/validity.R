4a9257a3851ea9e07857f1c6b6e296224555cc7c
MirkoTh <mirkothalmann@hotmail.com>
2024-05-16 15:22:45 +0200

    handle models without and with intercept

--- analysis/validity.R
+++ analysis/validity.R
@@ -23,11 +23,27 @@ tbl_exclude <- tbl_excl_wm %>%
   left_join(tbl_excl_bandits_2 %>% select(ID, exclude), by = c("participant_id" = "ID"), suffix = c("_1", "_2")) %>%
   mutate(exclude_all = ifelse((excl_subject + exclude_1 + exclude_2) > 0, TRUE, FALSE))
 
+fit_intercept <- FALSE
+path_horizon1 <- "analysis/bandits/modellingResults/fitHorizonSession1UCBfullno_horizon"
+path_horizon2 <- "analysis/bandits/modellingResults/fitHorizonSession2UCBfullno_horizon"
+path_sam1 <- "analysis/bandits/modellingResults/fitSamSession1UCBhierarchical"
+path_sam2 <- "analysis/bandits/modellingResults/fitSamSession2UCBhierarchical"
 
+if (!fit_intercept){
+  h1 <- str_c(path_horizon1, "no_intercept.Rda")
+  h2 <- str_c(path_horizon2, "no_intercept.Rda")
+  s1 <- str_c(path_sam1, "no_intercept.Rda")
+  s2 <- str_c(path_sam2, "no_intercept.Rda")
+} else if (fit_intercept) {
+  h1 <- str_c(path_horizon1, ".Rda")
+  h2 <- str_c(path_horizon2, ".Rda")
+  s1 <- str_c(path_sam1, ".Rda")
+  s2 <- str_c(path_sam2, ".Rda")
+}
 
-load("analysis/bandits/modellingResults/fitHorizonSession1UCBfullno_horizonno_intercept.Rda")
+load(h1)
 tbl_horizon_1 <- rename_and_add_id(trueParams, "baymodel")
-load("analysis/bandits/modellingResults/fitHorizonSession2UCBfullno_horizonno_intercept.Rda")
+load(h2)
 tbl_horizon_2 <- rename_and_add_id(trueParams, "baymodel")
 
 # load("analysis/bandits/modellingResults/fitHorizonSession1UCBfullno_horizon.Rda")
@@ -41,9 +57,9 @@ tbl_horizon <- tbl_horizon_1 %>%
   pivot_wider(names_from = predictor, values_from = c(estimate_1, estimate_2))
 
 
-load("analysis/bandits/modellingResults/fitSamSession1UCBhierarchicalno_intercept.Rda")
+load(s1)
 tbl_sam_1 <- rename_and_add_id(trueParams, "trueModel")
-load("analysis/bandits/modellingResults/fitSamSession2UCBhierarchicalno_intercept.Rda")
+load(s2)
 tbl_sam_2 <- rename_and_add_id(trueParams, "trueModel")
 
 tbl_sam <- tbl_sam_1 %>% 
@@ -67,6 +83,7 @@ tbl_bandits <- tbl_restless %>%
   filter(!is.na(estimate_1_V_sam)) %>%
   mutate(id = as.numeric(id))
 
+# re-code V and RU in Sam's task and Horizon task
 tbl_bandits <- tbl_bandits %>%
   mutate(
     estimate_1_V_sam = -1 * estimate_1_V_sam,
@@ -79,25 +96,32 @@ tbl_bandits <- tbl_bandits %>%
     estimate_2_RU_horizon = -1 * estimate_2_RU_horizon
   )
 
+
+colnames_ic <- c("V Restless", "RU Restless", "Intercept Sam", "V Sam", "RU Sam", "Intercept Horizon", "RU Horizon", "V Horizon")
+colnames_no_ic <- c("V Restless", "RU Restless", "V Sam", "RU Sam", "RU Horizon", "V Horizon")
+
 tbl_bandits_1 <- tbl_bandits %>%
   select(contains("1"))
-colnames(tbl_bandits_1) <- c("V Restless", "RU Restless", "V Sam", "RU Sam", "RU Horizon", "V Horizon")
 tbl_bandits_2 <- tbl_bandits %>%
   select(contains("2"))
-colnames(tbl_bandits_2) <- c("V Restless", "RU Restless", "V Sam", "RU Sam", "RU Horizon", "V Horizon")
+if (fit_intercept){
+  colnames(tbl_bandits_1) <- colnames(tbl_bandits_2) <- colnames_ic
+} else if (!fit_intercept)  {
+  colnames(tbl_bandits_1) <- colnames(tbl_bandits_2) <- colnames_no_ic
+}
 
-my_corr_plot <- function(cortable, x_lab, y_lab) {
+my_corr_plot <- function(cortable, x_lab, y_lab, ttl) {
   as.data.frame(cortable) %>%
     mutate(rwn = rownames(.)) %>%
     pivot_longer(-rwn) %>%
     ggplot(aes(fct_rev(rwn), name)) +
     geom_tile(aes(fill = value)) +
     geom_text(aes(label = round(value, 2)), color = "white") +
-    scale_fill_viridis_c() +
+    scale_fill_viridis_c(limits = c(0, 1)) +
     theme_bw() +
     scale_x_discrete(expand = c(0.01, 0)) +
     scale_y_discrete(expand = c(0.01, 0)) +
-    labs(x = x_lab, y = y_lab) + 
+    labs(x = x_lab, y = y_lab, title = ttl) + 
     theme(
       strip.background = element_rect(fill = "white"), 
       text = element_text(size = 22),
@@ -105,8 +129,8 @@ my_corr_plot <- function(cortable, x_lab, y_lab) {
     )
 }
 
-pl_cors_s1 <- my_corr_plot(cor(tbl_bandits_1), "Parameter 1", "Parameter 2")
-pl_cors_s2 <- my_corr_plot(cor(tbl_bandits_2), "Parameter 1", "Parameter 2")
+pl_cors_s1 <- my_corr_plot(cor(tbl_bandits_1), "Parameter 1", "Parameter 2", "Session 1")
+pl_cors_s2 <- my_corr_plot(cor(tbl_bandits_2), "Parameter 1", "Parameter 2", "Session 2")
 
 grid.draw(arrangeGrob(pl_cors_s1, pl_cors_s2, nrow = 1))
 
@@ -130,11 +154,11 @@ l_tbl_wm <- split(tbl_wm, tbl_wm$session)
 
 pl_cors_wm_s1 <- my_corr_plot(
   cor(l_tbl_wm[[1]] %>% select(-c(participant_id, session, processing_OS, processing_SS)) %>% rename("OS" = "recall_OS", "SS" = "recall_SS", "WMU" = "recall_WMU")),
-  "Measure 1", "Measure 2"
+  "Measure 1", "Measure 2", "Session 1"
 )
 pl_cors_wm_s2 <- my_corr_plot(
   cor(l_tbl_wm[[2]] %>% select(-c(participant_id, session, processing_OS, processing_SS)) %>% rename("OS" = "recall_OS", "SS" = "recall_SS", "WMU" = "recall_WMU")),
-  "Measure 1", "Measure 2"
+  "Measure 1", "Measure 2", "Session 2"
 )
 grid.draw(arrangeGrob(pl_cors_wm_s1, pl_cors_wm_s2, nrow = 1))
 
@@ -160,9 +184,9 @@ tbl_q_wide <- tbl_q %>%
   pivot_wider(
     id_cols = c(participant_id, session), 
     names_from = measure, values_from = score
-    ) %>% 
+  ) %>% 
   pivot_wider(id_cols = participant_id, names_from = session, values_from = c(BIG_5, CEI, PANASneg, PANASpos, PHQ_9, STICSA))
-  
+
 
 
 
@@ -247,3 +271,8 @@ lavaanPlot(
   node_options = list(shape = "box", fontname = "Helvetica")
 )
 
+
+
+
+cor(tbl_wm_bandits_1)
+cor(tbl_wm_bandits_2)
