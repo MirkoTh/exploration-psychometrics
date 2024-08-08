17174a24cbf18e478f31b34c4601aa3a58e66e1d
MirkoTh <mirkothalmann@hotmail.com>
2024-08-08 11:08:42 +0200

    initialize script to plot figures for ms

---
+++ analysis/plotting-ms.R
@@ -0,0 +1,35 @@
+# final plotting for MS
+
+
+library(grid)
+library(gridExtra)
+library(tidyverse)
+
+# Which Models? -----------------------------------------------------------
+
+my_dir <- "figures/figures-ms/submission-1"
+
+# how did we arrive at the used models?
+
+## restless bd ------------------------------------------------------------
+
+
+pl_restless_group_patterns <- readRDS("data/restless-group-patterns-across-methods.Rds")
+pl_restless_params_across_methods <- readRDS("data/restless-params-across-methods.RDS")
+tbl_restless_reliability_across_methods <- readRDS("data/restless-reliabilities-across-methods.Rds")
+
+table_rl_reliab <- tableGrob(tbl_restless_reliability_across_methods)
+pl_table_rl_reliab <- ggplot() + annotation_custom(table_rl_reliab)
+pl_arrive_restless_model <- arrangeGrob(
+  pl_restless_group_patterns, pl_restless_params_across_methods, pl_table_rl_reliab,
+  nrow = 1
+)
+
+if (!dir.exists(my_dir)){dir.create(my_dir, recursive = TRUE)}
+
+save_my_pdf_and_tiff(
+  pl_arrive_restless_model,
+  str_c(my_dir, "/arrive-restless-model"),
+  20, 7
+)
+
