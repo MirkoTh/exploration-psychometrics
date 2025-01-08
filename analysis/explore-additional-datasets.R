9c1115aeb0329c67c76d99a9f9de73e95602bba3
MirkoTh <mirkothalmann@hotmail.com>
2025-01-08 16:59:58 +0100

    add code to explore further Horizon task datasets

---
+++ analysis/explore-additional-datasets.R
@@ -0,0 +1,161 @@
+rm(list = ls())
+
+# Load Packages and Data --------------------------------------------------
+
+
+library(tidyverse)
+library(grid)
+library(gridExtra)
+library(rutils)
+
+dirs_homegrown <- c(
+  "utils/analysis-utils.R", "utils/plotting-utils.R", 
+  "utils/modeling-utils.R", "utils/retest-utils.R"
+)
+walk(dirs_homegrown, source)
+
+tbl_retest <- read_csv("data/bob-wilson/TEST-RETEST-2015_HorizonTask.csv") %>%
+  select(-c(repeatNumber, age, gender))
+# cols to drop: repeatNumber (always 1), age (range from 11-23), gender
+tbl_vaccine <- read_csv("data/bob-wilson/VACCINE-2015_HorizonTask.csv") %>%
+  select(-c(age, gender))
+# cols to drop: age, gender
+# question: what does repeatNumber mean?
+
+
+l_retest <- overview(tbl_retest)
+l_vaccine <- overview(tbl_vaccine)
+tbl_retest_elapsed <- time_elapsed_retest(l_retest$tbl_full) %>% mutate(Study = "Retest")
+tbl_vaccine_elapsed <- time_elapsed_vaccine(l_vaccine$tbl_full) %>% mutate(Study = "Vaccine")
+tbl_elapsed_both <- tbl_retest_elapsed %>% rbind(tbl_vaccine_elapsed)
+
+# plot test-retest times
+tbl_elapsed_both %>%
+  ggplot(aes(time_duration)) + 
+  geom_histogram(fill = "#66C2A5", color = "black", binwidth = 1) +
+  geom_label(
+    data = tbl_elapsed_both %>% count(Study), 
+    aes(x = 12, y = 22, label = str_c("n = ", n))) +
+  facet_wrap(~ Study) +
+  theme_bw() +
+  scale_x_continuous(breaks = seq(0, 15, by = 2), expand = c(0.01, 0)) +
+  scale_y_continuous(expand = c(0.01, 0)) +
+  labs(x = "Days Elapsed", y = "Nr. Participants") + 
+  theme(
+    strip.background = element_rect(fill = "white"), 
+    text = element_text(size = 22),
+    legend.position = "bottom"
+  ) +
+  coord_cartesian(xlim = c(0, 15))
+
+tbl_both_full_long <- bring_rewards_to_long(l_retest, l_vaccine)
+tbl_both_full_long <- running_means(tbl_both_full_long)
+
+only_first_frc <- tbl_both_full_long %>% filter(trial == 5)
+
+only_first_frc <- only_first_frc %>% 
+  mutate(
+    # running calculations
+    which_lower_mean = factor(is_lower_mean_2, labels = c(1, 2)),
+    which_higher_info = factor(is_higher_info_2, labels = c(1, 2)),
+    is_lower_mean = choice == which_lower_mean,
+    is_higher_info = choice == which_higher_info,
+    # ground truth (mean), and forced-choice info diff (info)
+    which_lower_mean_gt = factor(m2 < m1, labels = c(1, 2)),
+    which_higher_info_gt = factor(uc == 3, labels = c(1, 2)),
+    is_lower_mean_gt = choice == which_lower_mean_gt,
+    is_higher_info_gt = choice == which_higher_info_gt,
+    # numeric variables
+    mean_diff = running_mean_2 - running_mean_1,
+    mean_diff_bin = cut(mean_diff, c(-Inf, -25, seq(-16, 16, by = 4), 25, Inf))
+  )
+
+# plot for information is different than for unequal means,
+# because unequal info can only be calculated in 1-3 or 3-1 trials
+# i.e., not in 2-2 trials
+
+agg_means <- only_first_frc %>% 
+  #filter(n_choices_1 == 2 & n_choices_2 == 2) %>%
+  group_by(
+    expt_name, subjectID, sessionNum, gameLength
+  ) %>%
+  summarize(
+    is_lower_mean_avg = mean(is_lower_mean),
+  ) %>% ungroup()
+
+agg_info <- only_first_frc %>% 
+  filter(n_choices_1 != 2 & n_choices_2 != 2) %>%
+  group_by(
+    expt_name, subjectID, sessionNum, gameLength
+  ) %>%
+  summarize(
+    is_higher_info_avg = mean(is_higher_info)
+  ) %>% ungroup()
+
+agg_means_info <- left_join(agg_means, agg_info, by = c("expt_name", "subjectID", "sessionNum", "gameLength"))
+
+agg_plot <- agg_means_info %>% pivot_longer(c(is_lower_mean_avg, is_higher_info_avg)) %>%
+  pivot_wider(values_from = value, names_from = sessionNum, names_prefix = "session_")
+agg_plot$name <- factor(agg_plot$name, labels = c("p (high info)", "p (low mean)"))
+agg_plot$gameLength <- factor(agg_plot$gameLength, labels = c("Horizon 1", "Horizon 6"))
+
+ggplot(agg_plot, aes(session_1, session_2)) +
+  geom_abline() +
+  geom_point(shape = 1, size = 3) +
+  geom_smooth(method = "lm") +
+  facet_grid(name ~ gameLength) +
+  coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
+  theme_bw() +
+  scale_x_continuous(expand = c(0.01, 0), breaks = seq(.1, .9, by = .2)) +
+  scale_y_continuous(expand = c(0.01, 0)) +
+  labs(x = "Session 1", y = "Session 2") + 
+  theme(
+    strip.background = element_rect(fill = "white"), 
+    text = element_text(size = 22),
+    legend.position = "bottom"
+  )
+agg_plot$expt_name <- factor(agg_plot$expt_name, labels = c("Retest", "Vacc."))
+
+rutils::grouped_agg(agg_plot, c(expt_name, gameLength, name), c(session_1, session_2)) %>%
+  rename(
+    measure = name,
+    S1 = mean_session_1,
+    S2 = mean_session_2
+    ) %>%
+  pivot_longer(c("S1", "S2")) %>%
+  ggplot(aes(gameLength, value, group = gameLength)) +
+  geom_hline(yintercept = .5, color = "grey", linetype = "dotdash", linewidth = 1) +
+  geom_col(aes(fill = gameLength)) +
+  facet_grid(interaction(expt_name, name, sep = "-")  ~ measure) +
+  theme_bw() +
+  scale_x_discrete(expand = c(0.01, 0)) +
+  scale_y_continuous(expand = c(0.01, 0), breaks = c(.1, .5, .9)) +
+  coord_cartesian(ylim = c(0, 1)) +
+  labs(x = "Horizon", y = "P (Choice)") + 
+  theme(
+    strip.background = element_rect(fill = "white"), 
+    text = element_text(size = 22),
+    legend.position = "bottom"
+  ) + 
+  scale_fill_brewer(palette = "Set2", name = "")
+
+only_first_frc %>%
+  group_by(
+    expt_name, subjectID, sessionNum, gameLength, mean_diff_bin
+  ) %>%
+  summarize(
+    prop_high_mean = mean(choice == 2),
+    n = n()
+  ) %>%
+  group_by(
+    expt_name, sessionNum, gameLength, mean_diff_bin
+  ) %>%
+  summarize(
+    prop_high_mean = mean(prop_high_mean),
+    n = sum(n)
+  ) %>%
+  ggplot(aes(mean_diff_bin, prop_high_mean, group = gameLength)) +
+  geom_line(aes(color = gameLength)) +
+  geom_point(aes(color = gameLength, size = n)) +
+  facet_wrap(expt_name ~ sessionNum)
+
---
+++ utils/retest-utils.R
@@ -0,0 +1,175 @@
+
+overview <- function(tbl_df) {
+  #' @description summarizes how many participants completed how many sessions
+  #' @param tbl_df the tbl with the by-trial data
+  #' @return a list with
+  #' a) a summary of participants with one or two sessions
+  #' b) the details about who completed which session
+  #' c) which participants to keep and which to drop for test-retest analysis
+  #' d) the filtered original data set
+  tbl_design_retest <- tbl_df %>%
+    count(subjectID) %>% select(-n) %>%
+    crossing(sessionNum = c(1, 2))
+  
+  tbl_retest_ntrials <- tbl_df %>% 
+    count(subjectID, subjectNumber, sessionNum)
+  
+  tbl_overview <- tbl_design_retest %>% 
+    left_join(tbl_retest_ntrials, by = c("subjectID", "sessionNum"))
+  
+  tbl_keep <- tbl_overview %>% filter(!is.na(subjectNumber)) %>%
+    group_by(subjectID) %>%
+    summarize(n_sessions = length(subjectNumber))
+  
+  tbl_nsessions <- tbl_df %>% count(subjectID, subjectNumber, sessionNum) %>% 
+    arrange(subjectID, subjectNumber, sessionNum) %>%
+    count(subjectID) %>% count(n)
+  
+  tbl_full <- tbl_keep %>% filter(n_sessions == 2) %>%
+    select(-n_sessions) %>%
+    left_join(tbl_df, by = "subjectID")
+  
+  return(list(
+    tbl_nsessions = tbl_nsessions, 
+    tbl_overview = tbl_overview,
+    tbl_keep = tbl_keep,
+    tbl_full = tbl_full
+  ))
+}
+time_elapsed_retest <- function(tbl_full) {
+  #' @description extract time elapsed between test and retest
+  #' @param tbl_full tbl with by-trial data of kept participants
+  #' @return a tbl the elapsed time per participant with two sessions
+  tbl_full %>% group_by(subjectID) %>%
+    mutate(
+      date_session_prep1 = str_extract(file_name, ".*_.*_"),
+      date_session_prep2 = str_match(date_session_prep1, ".*_(.*)_")[,2],
+      session_day = as.numeric(str_extract(date_session_prep2, "^[0-9]+")),
+      session_month = str_match(date_session_prep2, "[0-9]+([a-zA-Z]*)")[,2],
+      session_year = as.numeric(str_extract(date_session_prep2, "[0-9]*$")),
+      session_date = str_c(session_day, "-", session_month, "-", session_year),
+      session_date2 = as.Date(session_date, format = "%d-%b-%Y")
+    ) %>%
+    count(subjectID, session_date2) %>% group_by(subjectID) %>%
+    mutate(rwn = row_number(session_date2)) %>%
+    ungroup() %>%
+    pivot_wider(names_from = rwn, values_from = session_date2, names_prefix = "session_") %>%
+    mutate(time_duration = session_2 - session_1)
+}
+time_elapsed_vaccine <- function(tbl_full) {
+  #' @description extract time elapsed between test and retest
+  #' @param tbl_full tbl with by-trial data of kept participants
+  #' @return a tbl the elapsed time per participant with two sessions
+  tbl_full %>% group_by(subjectID) %>%
+    mutate(
+      date_session_prep = str_match(file_name, ".*_(.*)_")[,2],
+      session_date = as.Date(date_session_prep, format = "%Y%m%d")
+    ) %>% 
+    count(subjectID, session_date) %>% group_by(subjectID) %>%
+    mutate(rwn = row_number(session_date)) %>%
+    ungroup() %>%
+    pivot_wider(names_from = rwn, values_from = session_date, names_prefix = "session_") %>%
+    mutate(time_duration = session_2 - session_1)
+}
+
+
+bring_rewards_to_long <- function(l_retest, l_vaccine) {
+  #' @description bring by-trial data from one col into several cols
+  #' @param l_retest list with preprocessed retest data
+  #' @param l_vaccine list with preprocessed vaccine data
+  #' @return a tbl with the data from all studies merged in long format
+  #' 
+  tbl_both_full <- l_retest$tbl_full %>% 
+    select(-c(file_name, subjectNumber)) %>% 
+    rbind(
+    l_vaccine$tbl_full %>% 
+      select(-c(file_name, vaccineSession, subjectNumber, repeatNumber))
+  )
+  
+  r_long <- tbl_both_full %>%
+    pivot_longer(cols = str_c("r", 1:10), names_to = "trial", values_to = "reward") %>%
+    select(-c(str_c("c", 1:10), str_c("rt", 1:10))) %>%
+    mutate(trial = str_extract(trial, "[0-9]+"))
+  c_long <- tbl_both_full %>%
+    pivot_longer(cols = str_c("c", 1:10), names_to = "trial", values_to = "choice") %>%
+    select(-c(str_c("r", 1:10), str_c("rt", 1:10))) %>%
+    mutate(trial = str_extract(trial, "[0-9]+"))
+  rt_long <- tbl_both_full %>%
+    pivot_longer(cols = str_c("rt", 1:10), names_to = "trial", values_to = "rt") %>%
+    select(-c(str_c("r", 1:10), str_c("c", 1:10))) %>%
+    mutate(trial = str_extract(trial, "[0-9]+"))
+  tbl_both_full_long <- r_long %>%
+    left_join(
+      c_long, by = c(
+        "subjectID", "expt_name", "sessionNum", "block",
+        "game", "gameLength", "uc", "m1", "m2", "gID", "trial"
+      )
+    ) %>% left_join(
+      rt_long, by = c(
+        "subjectID", "expt_name", "sessionNum", "block",
+        "game", "gameLength", "uc", "m1", "m2", "gID", "trial"
+      )
+    )
+  tbl_both_full_long <- tbl_both_full_long %>% filter(choice != "NaN")
+  return(tbl_both_full_long)
+}
+
+running_means <- function(tbl_both_full_long) {
+  tbl_both_full_long <- tbl_both_full_long %>%
+    group_by(expt_name, subjectID, sessionNum, block, game, gameLength, uc, m1, m2, gID) %>%
+    mutate(
+      running_mean_1 = NA, 
+      running_mean_2 = NA,
+      reward_1 = NA,
+      reward_2 = NA
+    )
+  tbl_both_full_long$reward_1[tbl_both_full_long$choice == 1] <- tbl_both_full_long$reward[tbl_both_full_long$choice == 1]
+  tbl_both_full_long$reward_2[tbl_both_full_long$choice == 2] <- tbl_both_full_long$reward[tbl_both_full_long$choice == 2]
+  
+  rm_1 <- tbl_both_full_long %>% 
+    filter(!is.na(reward_1)) %>% 
+    mutate(
+      running_mean_1 = cummean(reward_1),
+      n_choices_1 = cumsum(!is.na(reward_1))
+      )
+  
+  rm_2 <- tbl_both_full_long %>%
+    filter(!is.na(reward_2)) %>% 
+    mutate(
+      running_mean_2 = cummean(reward_2),
+      n_choices_2 = cumsum(!is.na(reward_2))
+    )
+  
+  tmp <- tbl_both_full_long %>% 
+    select(-c(running_mean_1, running_mean_2)) %>% 
+    left_join(
+      rm_1 %>% select(-c(reward_1, reward_2, choice, rt, reward, running_mean_2)), by = c(
+      "expt_name", "subjectID", "sessionNum", "block", "game", "gameLength", "uc", "m1", "m2", "gID", "trial"
+    )) %>% 
+    left_join(
+      rm_2 %>% select(-c(reward_1, reward_2, choice, rt, reward, running_mean_1)), by = c(
+      "expt_name", "subjectID", "sessionNum", "block", "game", "gameLength", "uc", "m1", "m2", "gID", "trial"
+    ))
+  tbl_out <- tmp %>% fill(
+    running_mean_1, .direction = "down"
+  ) %>% fill(
+    running_mean_2, .direction = "down"
+  )  %>% fill(
+    n_choices_1, .direction = "down"
+  )  %>% fill(
+    n_choices_2, .direction = "down"
+  ) %>% mutate(
+    running_mean_1 = lag(running_mean_1),
+    running_mean_2 = lag(running_mean_2),
+    n_choices_1 = lag(n_choices_1),
+    n_choices_2 = lag(n_choices_2),
+    trial = as.numeric(trial)
+    ) %>% ungroup()
+  
+  tbl_out <- tbl_out %>%
+    mutate(
+      is_lower_mean_2 = running_mean_2 < running_mean_1,
+      is_higher_info_2 = n_choices_2 < n_choices_1
+    )
+  return(tbl_out)
+}
